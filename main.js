755e1c0404e881237175678616607674189d836b
Neil-George <neilgeorgeuk@gmail.com>
2021-08-26 11:50:03 +0100

    fragment and vertex shaders added

--- main.js
+++ main.js
@@ -3,22 +3,39 @@ import * as THREE from '/js/three.module.js'
 const scene = new THREE.Scene()
 const camera = new THREE.PerspectiveCamera(75, innerWidth / innerHeight, 0.1, 1000)
 
-const renderer = new THREE.WebGLRenderer()
-
+const renderer = new THREE.WebGLRenderer(
+    {
+        antialias: true
+    }
+)
+renderer.setPixelRatio(window.devicePixelRatio)
 renderer.setSize(innerWidth, innerHeight)
 document.body.appendChild(renderer.domElement)
 
-const sphere = new THREE.Mesh(new THREE.SphereGeometry(5, 100, 100), new THREE.MeshBasicMaterial({
-    color: 0x008B8B
+const vertexShader = `
+void  main(){
+    gl_Position = vec4(1, 0, 0, 1);
+}`;
+
+const fragmentShader = `
+void  main(){
+    gl_FragColor = vec4(1, 0, 0, 1);
+}`;
+
+
+const base = new THREE.Mesh(new THREE.SphereGeometry(5, 100, 100), new THREE.ShaderMaterial({
+    vertexShader,
+    fragmentShader
     })
 )
 
-scene.add(sphere)
-camera.position.z = 20
+scene.add(base)
+camera.position.z = 10
 
 function animate() {
     requestAnimationFrame(animate)
     renderer.render(scene, camera)
+    base.rotation.y += 0.005
 }
 
 animate()
